285e6a019bb1723ba18726200f90413f0ea64e0c
Osvaldo Alves <oalvesxp@gmail.com>
2023-11-28 17:57:36 -0300

    Starting the configuration of app.php

---
+++ imc_web/src/app.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by VisualStudioCode.
+ * User: oalves
+ * Date: 28/11/2023
+ * Description: Calculador de IMC 
+ */
+
+session_start();
+require_once("../app/etc/env.php");
+
+if (isset($_POST['calcular'])) {
+    $peso = $_POST['peso'];
+    $altura = $_POST['altura'];
+    $imc = $peso / ($altura * $altura);
+    $imc = number_format($imc, 2, '.', '');
+    
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $contato = $_POST['contato'];
+    $data = $_POST['data'];
+    $done = true;
+
+    $_SESSION['imc'] = $imc;    
+    $_SESSION['done'] = $done;
+
+    // Query para armazenar os dados no banco
+    $mysql_query = "INSERT INTO pacientes (NOME, CPF, CONTATO, ALTURA, PESO, IMC, DATA_COLETA) VALUES ('$nome', '$cpf', '$contato', '$altura',  '$peso', '$imc', '$data')";
+    $result = mysqli_query($conn, $mysql_query);
+}
+
+?>
